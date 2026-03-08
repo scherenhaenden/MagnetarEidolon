bc1c128b03a7cda61011b77d23cd593813ad3217
scherenhaenden <scherenhaenden@gmail.com>
2026-03-08 19:44:42 +0100

    Update typescript-angular-skeleton/src/app/app.component.ts
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- typescript-angular-skeleton/src/app/app.component.ts
+++ typescript-angular-skeleton/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { ProjectContextService } from './core/services/project-context.service.j
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   public readonly title = 'Magnetar Angular Skeleton';
